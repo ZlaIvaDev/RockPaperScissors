470cd62b94d8b44bdb1d58fc7afd6282f14fa4d3
VanZdev <139448115+ZlaIvaDev@users.noreply.github.com>
2025-10-14 17:07:30 +0900

    added get_choice and get_com_choice functions, score and round variables, polished the code

--- javascript.js
+++ javascript.js
@@ -1,8 +1,18 @@
 console.log("hello from the other side")
 
-choice = prompt("enter your choice: ")
-function rps() {
-    
+let round = 1;
+let my_score = 0;
+let com_score = 0;
+let score = `score is :${my_score}:${com_score}`;
+
+function get_human_choice() {
+    const choice = prompt("enter your choice: ")
+    console.log(`your choice is ${choice}`)
+    return choice
+
+}
+
+function get_computer_choice() {
     let computer_choice = Math.random()
     if (computer_choice < 0.334) {
         computer_choice = 'rock'
@@ -12,10 +22,46 @@ function rps() {
         computer_choice = 'scissors'
     }
 
-    console.log(`your choice is : ${choice}`)
-    console.log(`comp choice is : ${computer_choice}`)
-    
-    
+    console.log(`com choice is ${computer_choice}`)
+    return computer_choice
+
+}
+
+function rps() {
+
+    console.log(`it's round ${round}`)
+    let me = get_human_choice()
+    let com = get_computer_choice()
+
+    if (me === com){
+        console.log('its a tie!')
+        console.log(`${my_score}:${com_score}`)
+        round++
+    } else if (me === 'rock' && com === 'paper' || me === 'paper' && com === 'scissors' || me === 'scissors' && com === 'rock') {
+        console.log(`${com} beats ${me}! You lose!`)
+        com_score++
+        console.log(`${my_score}:${com_score}`)
+        round++
+        
+    } else {
+        console.log(`${me} beats ${com}! You win!`)
+        my_score++
+        console.log(`${my_score}:${com_score}`)
+        round++
+
+    }
+}
+
+/* sledeci zadatak je da podesis Skor ! 
+
+first few tries, placeholder code 
+checking logic*/
+/*
+function rps() {
+
+
+    get_human_choice()
+    get_computer_choice()
 
     if (choice === computer_choice){
         console.log('its a tie!')
@@ -31,10 +77,53 @@ function rps() {
         console.log('rock beats scissors, Com wins!')
     } else if (choice === 'scissors' && computer_choice === 'paper') {
         console.log('scissors beat paper, YOU win!')
-    } /*else {
-        console.log("INVALID CHOICE!") -> not necessary atm
-    }*/
+    }
+}
+    */
+
+
+/*
+
+function rps() {
+
+    if (get_human_choice() === get_computer_choice()){
+        console.log('its a tie!')
+        round++
+        console.log(`${score}`)
+    } else if (get_human_choice() === 'rock' && get_computer_choice() === 'paper') {
+        console.log('paper beats rock, Com wins!')
+        round++
+        com_score++
+        console.log(`${score}`)
+    } else if (get_human_choice() === 'rock' && get_computer_choice() === 'scissors') {
+        console.log('rock beats scissors, YOU win!')
+        round++
+        my_score++
+        console.log(`${score}`)
+    } else if (get_human_choice() === 'paper' && get_computer_choice() === 'scissors') {
+        console.log('scissors beat paper, Com wins!')
+        round++
+        com_score++
+        console.log(`${score}`)
+    } else if (get_human_choice() === 'paper' && get_computer_choice() === 'rock') {
+        console.log('paper beats rock, YOU win!')
+        round++
+        my_score++
+        console.log(`${score}`)
+    } else if (get_human_choice() === 'scissors' && get_computer_choice() === 'rock') {
+        console.log('rock beats scissors, Com wins!')
+        round++
+        com_score++
+        console.log(`${score}`)
+    } else if (get_human_choice() === 'scissors' && get_computer_choice() === 'paper') {
+        console.log('scissors beat paper, YOU win!')
+        round++
+        my_score++
+        console.log(`${score}`)
+    } 
+
     }
 
 rps()
-/* sledeci zadatak je da podesis Skor ! */
+
+*/
